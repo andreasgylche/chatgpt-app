b2652d9e44f7c227ce5611d611944fe1e3bec89f
Andreas Vifert <andreasgylche@gmail.com>
2023-07-23 17:39:21 +0200

    user button dropdown -> logout

--- components/NewChat.jsx
+++ components/NewChat.jsx
@@ -1,9 +1,9 @@
-import { PlusIcon, ChatBubbleLeftIcon } from '@heroicons/react/24/solid';
+import { ChatBubbleLeftIcon } from '@heroicons/react/24/solid';
 
 function NewChat() {
   return (
     <button className="flex items-center w-full h-10 gap-2 px-4 transition-colors border rounded-lg border-zinc-800 hover:border-violet-500">
-      <ChatBubbleLeftIcon className="w-4 h-4" />
+      <ChatBubbleLeftIcon className="w-6 h-6 md:w-4 md:h-4" />
       <span className="hidden text-sm md:block">New Chat</span>
     </button>
   );
--- components/UserButton.jsx
+++ components/UserButton.jsx
@@ -1,15 +1,50 @@
 'use client';
 
 import { AvatarImage } from './ui/Avatar';
-import { useSession } from 'next-auth/react';
+import {
+  EllipsisHorizontalIcon,
+  ArrowLeftOnRectangleIcon,
+} from '@heroicons/react/24/solid';
+import { signOut, useSession } from 'next-auth/react';
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 
 function UserButton() {
   const { data: session } = useSession();
   return (
-    <button className="flex items-center gap-2 mt-auto">
-      <AvatarImage image={session?.user?.image} name={session?.user?.name} />
-      <span className="text-sm">{session?.user?.name}</span>
-    </button>
+    <div className="mt-auto">
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger asChild>
+          <button className="flex items-center justify-center w-full h-10 gap-2 px-2 rounded-lg select-none hover:bg-zinc-800">
+            <AvatarImage
+              image={session?.user?.image}
+              name={session?.user?.name}
+            />
+            <span className="hidden text-sm md:block">
+              {session?.user?.name}
+            </span>
+            <EllipsisHorizontalIcon className="hidden w-6 h-6 ml-auto text-sm md:block" />
+          </button>
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Portal>
+          <DropdownMenu.Content
+            side="top"
+            align="start"
+            sideOffset={8}
+            className="p-2 rounded-lg bg-zinc-800 w-[185px]"
+          >
+            <DropdownMenu.Item asChild>
+              <button
+                onClick={() => signOut()}
+                className="flex items-center w-full h-10 gap-2 px-4 text-sm rounded-lg hover:bg-red-500/20"
+              >
+                <ArrowLeftOnRectangleIcon className="w-6 h-6" />
+                Sign out
+              </button>
+            </DropdownMenu.Item>
+          </DropdownMenu.Content>
+        </DropdownMenu.Portal>
+      </DropdownMenu.Root>
+    </div>
   );
 }
 
